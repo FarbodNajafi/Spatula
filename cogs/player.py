3fea653bd11e4769181e883565210dbbc36d330c
Farbod Najafi <farbodnajafi@gmail.com>
2021-05-21 22:16:09 +0430

    Added reconnect parameters to ffmpeg

--- cogs/player.py
+++ cogs/player.py
@@ -17,11 +17,12 @@ ytdl_format_options = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
 ffmpeg_options = {
-    'options': '-vn'
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn',
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)

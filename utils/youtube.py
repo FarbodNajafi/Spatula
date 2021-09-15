258464024b7255a210d296ddde7a3ced20964e87
Farbod Najafi <farbodnajafi@gmail.com>
2021-09-15 12:39:17 +0430

    Increased -reconnect_delay_max

--- utils/youtube.py
+++ utils/youtube.py
@@ -21,13 +21,13 @@ class YTDLSource(discord.PCMVolumeTransformer):
         'ignoreerrors': False,
         'logtostderr': False,
         'quiet': True,
-        'no_warnings': True,
+        'no_warnings': False,
         'default_search': 'auto',
         'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
     }
 
     FFMPEG_OPTIONS = {
-        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 30',
         'options': '-vn',
     }
 
